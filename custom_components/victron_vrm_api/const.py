379368b50f207883a889d48b549369e94a7f1d6a
jayjojayson <2683358+jayjojayson@users.noreply.github.com>
2025-11-18 01:05:53 +0100

    update v_1.2

--- custom_components/victron_vrm_api/const.py
+++ custom_components/victron_vrm_api/const.py
@@ -10,7 +10,6 @@ CONF_BATTERY_INSTANCE = "battery_instance_id" # Instance ID der Batterie
 CONF_MULTI_INSTANCE = "multi_instance_id"     # Instance ID des MultiPlus (Status)
 
 # Standard-Aktualisierungsintervalle (in Sekunden)
-DEFAULT_SCAN_INTERVAL_BATTERY = 10     # 10 Sekunden
-DEFAULT_SCAN_INTERVAL_MULTI = 10       # 10 Sekunden 
+DEFAULT_SCAN_INTERVAL_BATTERY = 30     # 8 Sekunden
+DEFAULT_SCAN_INTERVAL_MULTI = 30       # 8 Sekunden 
 DEFAULT_SCAN_INTERVAL_OVERALL = 300    # 5 Minuten
-
